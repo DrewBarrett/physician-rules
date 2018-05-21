e9a9fc44e7263eb65a696c47d53f189a2b657fb6
Drew Barrett <dfb1998@gmail.com>
2018-05-20 21:04:54 -0400

    update models

--- addentry/models.py
+++ addentry/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Physician(models.Model):
-    npi = models.PositiveInt(help_text="A number between 1000000000 and 19999999999",
+    npi = models.PositiveIntegerField(help_text="A number between 1000000000 and 19999999999",
                              primary_key=True)
-    board_cert = models.
+    board_cert = models.BooleanField(help_text="Whether or not the doctor is board certified",
+                                     default=False)
+    age = models.PositiveSmallIntegerField(help_text="The doctors age")
+    rating = models.PositiveSmallIntegerField(help_text="A rating from 1 to 5",
+                                              default=4)
+    language = models.CharField(help_text="The doctors most proficient language",
+                                default="English",
+                                max_length=256)
+    location = models.CharField(help_text="The primary office location of this doctor",
+                                max_length=256)
--- physicianrules/settings.py
+++ physicianrules/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'addentry.apps.AddentryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
